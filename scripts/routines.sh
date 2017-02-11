a5a722cef48e04a026bd2e6d71f166077215f638
Atsushi Hashimoto <ahasimoto@mm.media.kyoto-u.ac.jp>
2017-02-11 20:46:25 +0900

    support qsub command.

--- scripts/routines.sh
+++ scripts/routines.sh
@@ -10,12 +10,38 @@ RESULTS_DIR=${EXP_DIR}/results
 TRIALS=`seq -f "%03g" 0 0`
 
 
-QSUB=0
+QSUB=1
 QSUB_DIR=${EXP_DIR}/qsub
 QSUB_COMMAND='qsub -ug gr20111 -q tc -A p=1:t=1:c=1:m=128M'
+UserGroup=gr20111
+Queue=gr20100b
+
 #EXE=sh\ -c
 EXE=echo
-#EXE=${QSUB_COMMAND}
+
+exec_command(){
+  comm=$1
+  if [ ${QSUB} -eq 0 ]; then
+    ${EXE} ${comm}
+    return
+  fi
+
+  if [ $# -gt 1 ]; then
+    option=$2
+  else
+    option="p=1:t=1:c=1:m=256M"
+  fi
+  if [ $# -gt 2 ]; then
+    hours=$3
+  else
+    hours=00:30
+  fi
+  #echo "/usr/bin/ls -lha > test.log" > temp|qsub -ug gr20111 -q gr20100b -W 12:00 -A p=1:t=1:c=1:m=1M temp
+  temp_id=$(date +"%s");
+  temp_sh=qsub_${temp_id}.sh
+  comm_="echo \"${comm}\" > ${temp_sh} | qsub -ug ${UserGroup} -q ${Queue} -W ${hours} -A ${option} ${temp_sh}"
+  ${EXE} ${comm_}
+}
 
 
 get_data(){
@@ -117,8 +143,9 @@ reduce_dimension(){
   for trial in ${TRIALS}; do
     local src_file=$(get_original_data ${exp} ${trial})
     local dist_file=$(get_data ${exp} ${trial} ${alg}/${dim})
-    #128MB
+    #128MB for 128dim x 1000samples
     #${EXE} "python tools/reduce_dimension.py ${dim} ${src_file} ${dist_file} --algorithm ${alg}"
+    exec_command "python tools/reduce_dimension.py ${dim} ${src_file} ${dist_file} --algorithm ${alg}"
   done
 }
 
@@ -133,6 +160,7 @@ sparse_encode(){
   for trial in ${TRIALS}; do
     local src_file=$(get_original_data ${exp} ${trial})
     local dist_file=$(get_data ${exp} ${trial} sparse_encode/${alpha})
-    ${EXE} "python tools/sparse_encoding.py ${alpha} ${src_file} ${dist_file}"
+    #196MB for 128dim x 1000samples
+    exec_command "python tools/sparse_encoding.py ${alpha} ${src_file} ${dist_file}"
   done
 }
