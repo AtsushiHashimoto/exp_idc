4b0f195aa523989f77caa56e1ce3d6b6058dac66
AtsushiHashimoto <ahasimoto@mm.media.kyoto-u.ac.jp>
2017-02-12 22:47:26 +0900

    fix bugs around destination directories.

--- scripts/conv2matrix.sh
+++ scripts/conv2matrix.sh
@@ -11,17 +11,17 @@ se_paths=`seq -f "sparse_encode/%g" 0.00 0.05 1.00`
 for exp in ${target_datasets}; do
   for src_subpath in raw ${se_paths}; do
     # cosine
-    make_affinity_matrix ${exp} cosine ${src_subpath} affinity_cosine
+    make_affinity_matrix ${exp} cosine ${src_subpath} ${src_subpath}/affinity_cosine
     # euclidean (rbf-kernel) with pair-variant gamma (derived from self-tuning spectral clustering)
-    make_affinity_matrix ${exp} euclidean ${src_subpath} affinity_stsc
+    make_affinity_matrix ${exp} euclidean ${src_subpath} ${src_subpath}/affinity_stsc
     # euclidean (rbf-kernel) with uniform gamma
-    make_affinity_matrix ${exp} euclidean ${src_subpath} affinity_euclidean "--gamma=0.001"
+    make_affinity_matrix ${exp} euclidean ${src_subpath} ${src_subpath}/affinity_euclidean "--gamma=0.001"
   done
 done
 pca_paths=pca/64
 for exp in ${target_datasets}; do
   for src_subpath in raw ${pca_paths}; do
-    make_distance_matrix ${exp} cosine ${src_subpath} distance_cosine
-    make_distance_matrix ${exp} euclidean ${src_subpath} distance_euclidean
+    make_distance_matrix ${exp} cosine ${src_subpath} ${src_subpath}/distance_cosine
+    make_distance_matrix ${exp} euclidean ${src_subpath} ${src_subpath}/distance_euclidean
   done
 done
