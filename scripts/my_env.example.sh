acbcfcef696bc7d5d3998fea466391a4c615781c
AtsushiHashimoto <ahasimoto@mm.media.kyoto-u.ac.jp>
2017-02-13 17:13:32 +0900

    add environmental variables.

---
+++ scripts/my_env.example.sh
@@ -0,0 +1,10 @@
+# flag to overwrite all calculation results.
+# OVERWRITE=1
+OVERWRITE=0
+
+# flag to generate qsub command for super computing systems (or any systems with job scheduler).
+QSUB=0
+
+# EXE=echo to print command, "sh\ -c" for execution.
+#EXE=sh\ -c
+EXE=echo
