acbff721ec707afc845f39e43ab327810a7ba71a
AtsushiHashimoto <ahasimoto@mm.media.kyoto-u.ac.jp>
2017-02-13 11:12:37 +0900

    a remained bug was fixed

--- scripts/make_dbs.sh
+++ scripts/make_dbs.sh
@@ -6,12 +6,12 @@ source scripts/routines.sh
 
 # dimension reduction
   #test: 100MB
-dist_dir=$(dirname $(get_original_data_dir test ))
+dist_dir=$(get_original_data_dir test)
 mkdir -p ${dist_dir}
 python tools/make_dbs.py test external/test ${dist_dir}
 
 for n in `seq -f "%02g" 5 15`; do
-  dist_dir=$(dirname $(get_original_data_dir face_feature_${n}))
+  dist_dir=$(get_original_data_dir face_feature_${n})
   mkdir -p ${dist_dir}
   python tools/make_dbs.py face_feature external/face_feature ${dist_dir} --n_clusters ${n}
 done
