eccf991b8ba024267910ef561cdba18e5bdb9368
AtsushiHashimoto <ahasimoto@mm.media.kyoto-u.ac.jp>
2017-02-14 10:36:36 +0900

    move target_datasets to my_env.

--- scripts/dimension_reduction.sh
+++ scripts/dimension_reduction.sh
@@ -4,11 +4,9 @@ cd $(dirname ${BASH_SOURCE[0]})/../
 
 source scripts/routines.sh
 
-#target_datasets=(test face)
-#target_datasets=face preid
-target_datasets=`seq -f "face_feature_%02g" 5 15` 
+
 # dimension reduction
-for exp in ${target_datasets}; do
+for exp in ${TARGET_DATASETS}; do
   #for alg in pca nmf; do
   for alg in pca; do
     #for dim in 32 64 128; do
@@ -20,9 +18,9 @@ done
 # sparse encoding
 method=lasso_lars
 
-for exp in ${target_datasets}; do
+for exp in ${TARGET_DATASETS}; do
   # 0.0 to 1.0 with step 0.05
-  for alpha in `seq 0.00 0.05 1.00`; do
+  for alpha in `seq -f "%0.2f" 0.00 0.05 1.00`; do
     sparse_encode ${exp} ${alpha} ${metric} ${method}
   done
 done
