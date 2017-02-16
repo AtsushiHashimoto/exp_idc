c948444c86d808829148f152cbe1bf66cf835bd9
AtsushiHashimoto <ahasimoto@mm.media.kyoto-u.ac.jp>
2017-02-17 07:26:49 +0900

    complete SC, IDC, and DBSCAN in clustering scripts.

--- scripts/clustering.sh
+++ scripts/clustering.sh
@@ -12,54 +12,49 @@ for dataset in ${TARGET_DATASETS}; do
     echo "ERROR: no cluster number is obtained from dataset name."
     exit
   fi
-  echo ${dataset}
-  echo raw/affinity
-  echo $(get_matrix_dir ${dataset} raw/affinity_\*)
 
   # SC, IDC
   for mat in `find $(get_matrix_dir ${dataset} raw/affinity_\*) -type f|grep W_000.csv`; do
       src_dir=$(dirname ${mat})
       subpath=$(echo ${src_dir}|awk -F"${dataset}/" '{print $2}')
-      clustering ${dataset} sc ${subpath} ${subpath} "--n_clusters ${n_clusters}"
-      echo "expr ${n_clusters} + 1"
-      clustering ${dataset} sc ${subpath} ${subpath} "--n_clusters $(expr ${n_clusters} + 1)"
-      clustering ${dataset} idc ${subpath} ${subpath} "--min_clusters 3 --max_clusters 20"
+      clustering ${dataset} SC ${subpath} ${subpath}/SC_N "--n_clusters $(expr ${n_clusters})"
+      clustering ${dataset} SC ${subpath} ${subpath}/SC_N1 "--n_clusters $(expr ${n_clusters} + 1)"
+      clustering ${dataset} IDC ${subpath} ${subpath}/IDC "--min_clusters 3 --max_clusters 20"
+      # SG, MODULARITY, SEA
+      clustering ${dataset} SG ${subpath} ${subpath}/SG
+      clustering ${dataset} MODULARITY ${subpath} ${subpath}/MODULARITY
+      clustering ${dataset} SEA ${subpath} ${subpath}/SEA
   done
-
+  exit
   ####################
-  continue
 
   # Sparse SC
-  for mat in `find $(get_matrix_dir ${dataset} sparse_encode/affinity_\*) -type f|grep W_000.csv`; do
+  for se_path in ${se_paths}; do
+    for mat in `find $(get_matrix_dir ${dataset} ${se_path}/affinity_\*) -type f|grep W_000.csv`; do
       src_dir=$(dirname ${mat})
       subpath=$(echo ${src_dir}|awk -F"${dataset}/" '{print $2}')
-      clustering ${dataset} sc ${subpath} ${subpath} "--n_clusters ${n_clusters}"
-      clustering ${dataset} sc ${subpath} ${subpath} "--n_clusters $(expr ${n_clusters} + 1)"
+      clustering ${dataset} SC ${subpath} ${subpath}/SSC_N "--n_clusters $(expr ${n_clusters})"
+      clustering ${dataset} SC ${subpath} ${subpath}/SSC_N1 "--n_clusters $(expr ${n_clusters} + 1)"
+    done
   done
 
   # Distance Based Clustering Methods
 
   # DBSCAN
-  for src_type in raw pca/64; do
+  for src_type in raw ${pca_paths}; do
     for mat in `find $(get_matrix_dir ${dataset} ${src_type}/distance_\*) -type f|grep W_000.csv`; do
       src_dir=$(dirname ${mat})
       subpath=$(echo ${src_dir}|awk -F"${dataset}/" '{print $2}')
-      for param in `seq -f "%0.2f" 0.0 0.05 1.0`; do
+      for eps in dbscan_epss; do
         #######
-        clustering ${dataset} dbscan ${subpath} ${subpath}/${param} "--param ${param}"
+        clustering ${dataset} DBSCAN ${subpath} ${subpath}/DBSCAN/${eps} "--eps ${eps}"
       done
     done
   done
 
   # STSC
   for mat in `find $(get_matrix_dir ${dataset} raw/distance_euclidean) -type f|grep W_000.csv`; do
-    clustering ${dataset} stsc ${subpath} ${subpath} "--n_clusters "
+    clustering ${dataset} STSC ${subpath} ${subpath}/STSC "--n_clusters "
   done
 
-#  for src_subpath in affinity_euclidean; do
-#    cross_validation ${dataset} spectral_clustering ${src_subpath} "--n_clusters=${n_clusters} --params_from=directories --params_dir='sparse_encode:affinity_euclidean"
-#    for dim in 64; do
-#      cross_validation ${dataset}/pca/${dim} dbscan ${src_subpath}
-#    done
-#  done
 done
--- tools/do_clustering.py
+++ tools/do_clustering.py
@@ -12,20 +12,36 @@ from os.path import basename
 from os.path import dirname
 import sys
 
+
 sys.path.append(dirname(__file__))
-import my_target_counter
-import self_tuning_spectral_clustering
+from my_target_counter import TargetCounter
+
+logger = logging.getLogger(__file__)
 
 
+# HOW TO ADD NEW algorithm
+# 1. add a process for your algorithm to 'get_model'
+# 2. register your algorithm to the following list if distance based.
+DIST_BASED_ALGORITHMs = ["DBSCAN","STSC"]
+# 3. add a process to 'my_fit_predict' if the algorithm has outlier cluster and the outlier label is not 0.
+
+from memory_profiler import profile
 @profile
 def main(args):
     src_dir = args.src_dir
     dest_dir = args.dest_dir
-    src_pat = "X_(\d{3}).csv$"
+    print(dest_dir)
+
+    if args.algorithm in DIST_BASED_ALGORITHMs:
+        src_pat = "D_(\d{3}).csv$"
+    else:
+        src_pat = "W_(\d{3}).csv$"
+
     tar_template = "y_%s.dat"
     tc=TargetCounter(src_pat,tar_template,src_dir,dest_dir)
     target_ids,src_files = tc.listup_targets()
     n_targets = len(target_ids)
+
     if args.count_targets:
         print(len(target_ids))
         sys.exit()
@@ -34,27 +50,74 @@ def main(args):
         sys.exit()
 
     model = get_model(args)
+    for id,src_file in zip(target_ids,src_files):
+        dest_file = "%s/%s"%(args.dest_dir,tc.id2destfile(id))
+        X = np.loadtxt(src_file,delimiter=",")
+        y = my_fit_predict(model,X,args)
+        np.savetxt(dest_file,y)
 
-    for t in targets:
-        X=np.load("%s/X_%s.csv"%(src_dir,t))
-        y=model.fit_predict(X)
-        np.save("%s/%s"%(dest_dir,id2destfile(t)),y)
+def my_fit_predict(model,X,args):
+    alg = args.algorithm
+    y = model.fit_predict(X)
+    if alg == 'DBSCAN':
+        y = y+1 # DBSCAN uses -1 as outlier label, we treat 0 as the label.
+
+    return y
 
 def get_model(args):
     alg = args.algorithm
-    if alg=='IDC':
-
-    elif arg=='SC':
-        model =
-    elif arg=='DBSCAN':
-    elif arg=='MODULARITY':
+    if alg=='SC':
+        from sklearn.cluster import SpectralClustering
+        model = SpectralClustering(\
+                    n_clusters=args.n_clusters,\
+                    eigen_solver='arpack',\
+                    random_state=None,\
+                    affinity='precomputed',\
+                    assign_labels='discretize',
+                    n_jobs=1)
+    elif alg=='IDC':
+        from isolated_dense_clustering import IsolatedDenseClustering
+        search_range = range(args.min_clusters,args.max_clusters)
+        model = IsolatedDenseClustering(\
+                                  search_range=search_range, \
+                                  affinity='precomputed', \
+                                  assign_labels='discretize',\
+                                  n_jobs=1,\
+                                  eigen_solver='arpack', \
+                                  random_state=None)
+    elif alg=='SG':
+        pass
+    elif alg=='STSC':
+        import self_tuning_spectral_clustering
+        pass
+    elif alg=='MODULARITY':
+        pass
+    elif alg=='SEA':
+        pass
+    elif alg=='DBSCAN':
+        from sklearn.cluster import dbscan
+        model = DBSCAN(
+            eps=args.eps,
+            min_samples=args.min_samples,
+            metric="precomputed")
     else:
         logger.warn("Unknown Algorithm '%s' is directed."%alg)
-
+        sys.exit()
+    return model
 
 
 
 parser = argparse.ArgumentParser(description=DESCRIPTION)
+
+parser.add_argument('algorithm', \
+        action='store', \
+        nargs=None, \
+        const=None, \
+        default=None, \
+        type=str, \
+        choices=None, \
+        help='Clustering algorithm (SC|IDC|SG|STSC|MODULARITY|SEA|DBSCAN).', \
+        metavar=None)
 
 parser.add_argument('src_dir', \
         action='store', \
@@ -76,7 +139,7 @@ parser.add_argument('dest_dir', \
         help='Directory path where the formatted data will be located.', \
         metavar=None)
 
-parser.add_argument('--n_clusters' \
+parser.add_argument('--n_clusters', \
         action='store', \
         nargs=None, \
         const=None, \
@@ -85,8 +148,50 @@ parser.add_argument('--n_clusters' \
         choices=None, \
         help='Number of clusters.', \
         metavar=None)
+parser.add_argument('--min_clusters',\
+        action='store', \
+        nargs=None, \
+        const=None, \
+        default=None, \
+        type=int, \
+        choices=None, \
+        help='Minimum number of clusters to set the search range.', \
+        metavar=None
+        )
+parser.add_argument('--max_clusters',\
+        action='store', \
+        nargs=None, \
+        const=None, \
+        default=None, \
+        type=int, \
+        choices=None, \
+        help='Maximum number of clusters to set the search range.', \
+        metavar=None
+        )
 
+parser.add_argument('--eps',\
+        action='store', \
+        nargs=None, \
+        const=None, \
+        default=None, \
+        type=float, \
+        choices=None, \
+        help='eps for DBSCAN.', \
+        metavar=None
+        )
+parser.add_argument('--min_samples',\
+        action='store', \
+        nargs=None, \
+        const=None, \
+        default=None, \
+        type=int, \
+        choices=None, \
+        help='min_samples for DBSCAN.', \
+        metavar=None
+        )
 
+parser.add_argument('--count_targets',\
+        action="store_true", default=False, help='count processing targets, and exit.')
 
 
 if __name__ == '__main__':
